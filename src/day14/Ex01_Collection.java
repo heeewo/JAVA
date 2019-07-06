7ffee55ac272548ea088b2d94fa803459fd3efce
heeewo <esc5411@naver.com>
2019-07-06 17:45:29 +0900

    Add day14, Ex01_Coleection.java

---
+++ src/day14/Ex01_Collection.java
@@ -0,0 +1,24 @@
+package day14;
+
+public class Ex01_Collection {
+
+	public static void main(String[] args) {
+		/*
+		 * 		Java Collection Framework
+		 * 
+		 * 	Collection : 여러 객체를 묶어서 담을 수 있는 자료형
+		 * 	Framework : 어떤 문제에 대해 정형화 된 방법
+		 * 
+		 * 	Collection Framework : 여러 자료를 묶어서 관리하는 정형화된 클래스
+		 * 
+		 * 	1. 다양한 객체를 저장할 수 있어야 하기 때문에, 기본적으로 Object를 처리한다
+		 * 	2. 중복을 허용하거나 허용하지 않는 기준으로 분류할 수 있다
+		 * 	3. 순서가 있거나, 순서가 없는 기준으로도 분류 가능하다
+		 * 	4. Generic Type을 도입하여 원하는 자료형만 처리하도록 변경할 수 있다
+		 * 	5. 객체 타입이므로, 필드에 접근하는 경우는 거의 없고, 메서드를 이용한다
+		 * 
+		 * 	ArrayList, Set, Map
+		 */
+	}
+
+}
